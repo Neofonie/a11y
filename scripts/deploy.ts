037a4a9ba8b5c6ada9ab01e921d64004bc891238
woitas <dutscher_sbf@hotmail.com>
2024-02-17 23:52:14 +0100

    some typescriptinger

---
+++ scripts/deploy.ts
@@ -0,0 +1,34 @@
+import { Buffer } from 'buffer';
+import Client from 'ssh2-sftp-client';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+interface Secrets {
+    host: string,
+    port: number,
+    username: string,
+    passphrase: string,
+    privateKey: string,
+    destination: string,
+}
+
+const secrets = yargs(hideBin(process.argv)).argv as unknown as Secrets;
+const sftp = new Client();
+const src = './build';
+
+sftp
+    .connect({
+        host: secrets.host,
+        port: secrets.port,
+        username: secrets.username,
+        passphrase: secrets.passphrase,
+        privateKey: Buffer.from(secrets.privateKey.replace(/\|/g, '\n'), 'utf-8'),
+    })
+    .then(() => sftp.uploadDir(src, secrets.destination))
+    .then((msg: string) => {
+        console.log('Upload done', msg);
+        return sftp.end();
+    })
+    .catch((err: string) => {
+        console.log('Upload error', err,);
+    });

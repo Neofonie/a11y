03d1e00fc2d53ae73c96639c0d3ea6d7c90a5f8f
Dagmar Climent <dagmar.climent@neofonie.de>
2024-05-02 12:15:33 +0200

    checkbox cleaned up + spelling errors corrected

--- src/lib/components/checkbox.svelte
+++ src/lib/components/checkbox.svelte
@@ -1,21 +1,10 @@
 <script type="ts">
-    export let test = "456";
+    //export let test = "456";
 </script>
 
 <style>
-    .checkbox {
-      &.slot {
-        background: red;
-      }
-    }
 </style>
 
-<label class="checkbox">
-    <input type="checkbox" />
-    {test}
-    <div><slot /></div>
-
-    <div class="checkbox slot">
-    <slot name="named" />
-    </div>
+<label>
+    <input type="checkbox"/><slot />
 </label>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,60 +3,58 @@
     import Github from '$lib/components/github.svelte';
 </script>
 
+<h1>Test Accessibility „A11y“ (11 stehen für 11 Buchstaben, ergo eine Abkürzung)</h1>
+
 <Github/>
 
-<h1>Test Accessibility „A11y“ (11 stehen für 11 Buchstaben ergo eine abkürzung)</h1>
-
-<p>Diese Checkliste wird von unseren Entwicklern genutzt um eine gewisse Barriere-Freiheit für unsere Endkunden zu gewährleisten.
+<p>Diese Checkliste wird von unseren Entwicklern genutzt, um eine gewisse Barriere-Freiheit für unsere Endkunden zu gewährleisten.
 Jede Komponente/Seite sollte gegen diese Checkliste geprüft werden.</p>
 
 <h2>Allgemein</h2>
-<Checkbox test="123">
-    Alle automatischen Accessibility-Tests laufen durch
-    <div slot="named">huhu</div>
-</Checkbox> <br />
-<label><input type="checkbox">Axe Chrome extension</label> <br />
-<label><input type="checkbox">Wave Chrome Erweiterung</label> <br />
-<label><input type="checkbox">Lighthouse (Accessibility)</label> <br />
+
+<Checkbox>Alle automatischen Accessibility-Tests laufen durch</Checkbox> <br />
+<Checkbox>Axe Chrome Extension</Checkbox> <br />
+<Checkbox>Wave Chrome Extension</Checkbox> <br />
+<Checkbox>Lighthouse (Accessibility)</Checkbox> <br />
 
 <h2>Markup</h2>
 
-<label><input type="checkbox">`&lt;html />` hat korrektes lang-attribut</label> <br />
-<label><input type="checkbox">Es gibt bei tabindex nur 0 oder -1</label> <br />
-<label><input type="checkbox">`&lt;a />-Tag` wird für alle Links verwendet -> Seitenwechseln</label> <br />
-<label><input type="checkbox">`&lt;button />-Tag` wird für alle Buttons/Klick/Actionflächen genutzt (kein div mit onclick etc.)</label> <br />
+<Checkbox>`&lt;html />` hat korrektes lang-Attribut</Checkbox> <br />
+<Checkbox>Es gibt bei tabindex nur 0 oder -1</Checkbox> <br />
+<Checkbox>`&lt;a />-Tag` wird für alle Links verwendet -> Seitenwechsel</Checkbox> <br />
+<Checkbox>`&lt;button />-Tag` wird für alle Buttons, Klick- und Action-Flächen genutzt (kein div mit onclick etc.)</Checkbox> <br />
 
 <h2>Content</h2>
 
-<label><input type="checkbox">`&lt;button />`, `&lt;a />` und `&lt;label />` sind eindeutig und selbsterklärend</label> <br />
-<label><input type="checkbox">Es gibt nur eine `&lt;h1 />` pro Seite</label> <br />
+<Checkbox>`&lt;button />`, `&lt;a />` und `&lt;label />` sind eindeutig und selbsterklärend</Checkbox> <br />
+<Checkbox>Es gibt nur eine `&lt;h1 />` pro Seite</Checkbox> <br />
 
 <h2>Bilder</h2>
 
-<label><input type="checkbox">Alle `&lt;img />-Elemente` haben ein Alt-Attribut</label> <br />
-<label><input type="checkbox">rein dekorative SVGs haben `aria-hidden="true"`</label> <br />
-<label><input type="checkbox">`&lt;img />-Elemente` mit source SVG haben das role Attribut `role="img"`</label> <br />
+<Checkbox>Alle `&lt;img />-Elemente` haben ein alt-Attribut</Checkbox> <br />
+<Checkbox>rein dekorative SVGs haben `aria-hidden="true"`</Checkbox> <br />
+<Checkbox>`&lt;img />-Elemente` mit source SVG haben das role-Attribut `role="img"`</Checkbox> <br />
 
 <h2>Formulare</h2>
 
-<label><input type="checkbox">Alle Inputs sind mit entsprechenden Label verbunden</label> <br />
-<label><input type="checkbox">Form Input unterstützen grundsätzlich autocomplete</label> <br />
-<label><input type="checkbox">Input Errors werden direkt mit den entsprechenden Feldern assoziiert</label> <br />
+<Checkbox>Alle Inputs sind mit einem entsprechenden Label verbunden</Checkbox> <br />
+<Checkbox>Form-Inputs unterstützen grundsätzlich Autovervollständigung</Checkbox> <br />
+<Checkbox>Eingabefehler werden direkt mit den entsprechenden Feldern assoziiert</Checkbox> <br />
 
-<h2>Mobile</h2>
+<h2>Mobil</h2>
 
-<label><input type="checkbox">Die Seite kann nicht horizontal gescrollt werden</label> <br />
-<label><input type="checkbox">Viewport Zoom ist deaktiviert</label> <br />
-<label><input type="checkbox">Alle Klickflächen mind. `44x44px`, außer bei Inline-Elementen wie Links</label> <br />
+<Checkbox>Die Seite kann nicht horizontal gescrollt werden</Checkbox> <br />
+<Checkbox>Viewport Zoom ist deaktiviert</Checkbox> <br />
+<Checkbox>Alle Klick-Flächen mind. `44x44px`, außer bei Inline-Elementen wie Links</Checkbox> <br />
 
 <h2>Tastatur</h2>
 
-<label><input type="checkbox">Tastatursteuerung muss komplett möglich sein</label> <br />
-<label><input type="checkbox">Alle interaktiven Elemente müssen einen sichtbaren focustyle haben</label> <br />
-<label><input type="checkbox">Keyboard-Focus Reihenfolge gleicht der des visuellen Layouts</label> <br />
-<label><input type="checkbox">Es gibt keine unsichtbaren fokusierbaren Elemente</label> <br />
-<label><input type="checkbox">Buttons können über Leertaste und Eingabetaste betätigt werden</label> <br />
-<label><input type="checkbox">Menüs und Overlays können via ESC-Taste wieder geschlossen werden</label> <br />
+<Checkbox>Tastatursteuerung muss komplett möglich sein</Checkbox> <br />
+<Checkbox>Alle interaktiven Elemente müssen einen sichtbaren focustyle haben</Checkbox> <br />
+<Checkbox>Keyboard-Focus-Reihenfolge gleicht der des visuellen Layouts</Checkbox> <br />
+<Checkbox>Es gibt keine unsichtbaren fokusierbaren Elemente</Checkbox> <br />
+<Checkbox>Buttons können über Leertaste und Eingabetaste betätigt werden</Checkbox> <br />
+<Checkbox>Menüs und Overlays können via ESC-Taste wieder geschlossen werden</Checkbox> <br />
 
 
 <p><strong>Stand 02.2024</strong></p>

69fedf0da2ca6d0c71a56f2295f4879dad4c9542
dagmarcliment <166798148+dagmarcliment@users.noreply.github.com>
2024-05-02 16:36:27 +0200

    Feature/research and check spelling (#15)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-    import StoredCheckbox from "$lib/components/stored-checkbox.svelte";
-    import Github from "$lib/components/github.svelte";
-    import BuildDate from '$lib/components/buildDate.svelte';
+  import BuildDate from '$lib/components/buildDate.svelte';
+  import Github from "$lib/components/github.svelte";
+  import Link from "$lib/components/link.svelte";
+  import StoredCheckbox from "$lib/components/stored-checkbox.svelte";
 </script>
 
 <svelte:head>
@@ -12,14 +13,20 @@
 
 <div class="container mx-auto px-4 py-4">
     <h1>
-        Test Accessibility „A11y“ (11 stehen für 11 Buchstaben ergo eine abkürzung)
+      Test Accessibility „A11y“ (11 stehen für 11 Buchstaben, ergo eine Abkürzung)
     </h1>
 
     <p>
-        Diese Checkliste wird von unseren Entwicklern genutzt um eine gewisse
-        Barriere-Freiheit für unsere Endkunden zu gewährleisten. Jede Komponente/Seite
-        sollte gegen diese Checkliste geprüft werden.
+        Diese Checkliste wird von unseren Entwicklern genutzt, um eine gewisse Barriere-Freiheit für unsere Endkunden zu gewährleisten.
+        Jede Komponente/Seite sollte gegen diese Checkliste geprüft werden.
     </p>
+
+    <h2 class="mt-4 mb-1">Links</h2>
+
+    <Link href="https://www.w3schools.com/accessibility/index.php">W3School Accessibility Tutorial</Link>
+    <Link href="https://developer.mozilla.org/en-US/docs/Learn/Accessibility">MDN Dev Docs: Accessibility</Link>
+    <Link href="https://www.w3.org/TR/WCAG20/">Web Content Accessibility Guidelines (WCAG)</Link>
+    <Link href="https://www.barrierefreiheit-dienstekonsolidierung.bund.de/Webs/PB/DE/barrierefreie_it/digitale-barrierefreiheit/digitale-barrierefreiheit-node.html">Digitale Barrierefreiheit (Bundesministerium des Innern und für Heimat)</Link>
 
     <h2 class="mt-4 mb-1">Allgemein</h2>
 
@@ -28,13 +35,13 @@
         <div slot="named">huhu</div>
     </StoredCheckbox>
 
-    <StoredCheckbox key="axe-chrome-extension" label="Axe Chrome extension" />
-    <StoredCheckbox key="wave-chrome-extension" label="Wave Chrome Erweiterung" />
+    <StoredCheckbox key="axe-chrome-extension" label="Axe Chrome Extension" />
+    <StoredCheckbox key="wave-chrome-extension" label="Wave Chrome Extension" />
     <StoredCheckbox key="lighthouse" label="Lighthouse (Accessibility)" />
 
     <h2 class="mt-4 mb-1 ">Markup</h2>
 
-    <StoredCheckbox key="html-lang" label="`&lt;html />` hat korrektes lang-attribut" />
+    <StoredCheckbox key="html-lang" label="`&lt;html />` hat korrektes lang-Attribut" />
 
     <playground-ide editable-file-system line-numbers resizable>
         <script type="sample/html" filename="index.html">
@@ -53,8 +60,11 @@
     <StoredCheckbox key="tabindex" label="Es gibt bei tabindex nur 0 oder -1" />
     <StoredCheckbox key="a-tag" label="`&lt;a />-Tag` wird für alle Links verwendet -> Seitenwechseln" />
     <StoredCheckbox key="button-tag"
-        label="`&lt;button />-Tag` wird für alle Buttons/Klick/Actionflächen genutzt (kein div mit onclick etc.)" />
-
+        label="`&lt;button />-Tag` wird für alle Buttons, Klick- und Action-Flächen genutzt (kein div mit onclick etc.)" />
+    <StoredCheckbox
+        key="ul-ol-tags"
+        label="Wenn ein `&lt;ol>`- oder `&lt;ul>-Element` vorhanden ist, sollte es mindestens ein `&lt;li>-Element` enthalten. Wenn ein `&lt;dl>-Element` vorhanden ist, sollte es mindestens ein `&lt;dt>-Element` enthalten."
+    />
 
     <h2 class="mt-4 mb-1">Content</h2>
 
@@ -63,31 +73,37 @@
     <StoredCheckbox key="h1-per-page" label="Es gibt nur eine `&lt;h1 />` pro Seite" />
 
     <h2 class="mt-4 mb-1">Bilder</h2>
-
     <StoredCheckbox key="img-alt" label="Alle `&lt;img />-Elemente` haben ein Alt-Attribut" />
-    <StoredCheckbox key="svg-aria-hidden" label="rein dekorative SVGs haben `aria-hidden='true'`" />
+    <StoredCheckbox key="svg-aria-hidden" label="rein dekorative SVGs haben `aria-hidden=&quot;true&quot;`" />
     <StoredCheckbox key="img-svg-role"
-        label="`&lt;img />-Elemente` mit source SVG haben das role Attribut `role='img'`" />
+        label="&lt;img />-Elemente` mit source SVG haben das role-Attribut `role=&quot;img&quot;`" />
+
+    <h2 class="mt-4 mb-1">Video & Audio</h2>
+
+    <StoredCheckbox
+        key="videos-audios-playable"
+        label="Die automatische Wiedergabefunktion für Video (sowie für Audio oder andere Multimedia) ist entfernt. Benutzer können die Wiedergabe  starten, anhalten und stoppen."
+    />
 
     <h2 class="mt-4 mb-1">Formulare</h2>
 
-    <StoredCheckbox key="inputs-labels" label="Alle Inputs sind mit entsprechenden Label verbunden" />
-    <StoredCheckbox key="form-input-autocomplete" label="Form Input unterstützen grundsätzlich autocomplete" />
-    <StoredCheckbox key="input-errors" label="Input Errors werden direkt mit den entsprechenden Feldern assoziiert" />
+    <StoredCheckbox key="inputs-labels" label="Alle Inputs sind mit einem entsprechenden Label verbunden" />
+    <StoredCheckbox key="form-input-autocomplete" label="Form-Inputs unterstützen grundsätzlich Autovervollständigung" />
+    <StoredCheckbox key="input-errors" label="Eingabefehler werden direkt mit den entsprechenden Feldern assoziiert" />
 
-    <h2 class="mt-4 mb-1">Mobile</h2>
+    <h2 class="mt-4 mb-1">Mobil</h2>
 
     <StoredCheckbox key="no-horizontal-scroll" label="Die Seite kann nicht horizontal gescrollt werden" />
     <StoredCheckbox key="viewport-zoom" label="Viewport Zoom ist deaktiviert" />
     <StoredCheckbox key="click-areas-size"
-        label="Alle Klickflächen mind. `44x44px`, außer bei Inline-Elementen wie Links" />
+        label="Alle Klick-Flächen mind. `44x44px`, außer bei Inline-Elementen wie Links" />
 
     <h2 class="mt-4 mb-1">Tastatur</h2>
 
     <StoredCheckbox key="keyboard-control" label="Tastatursteuerung muss komplett möglich sein" />
     <StoredCheckbox key="interactive-elements-focus"
         label="Alle interaktiven Elemente müssen einen sichtbaren focustyle haben" />
-    <StoredCheckbox key="keyboard-focus-order" label="Keyboard-Focus Reihenfolge gleicht der des visuellen Layouts" />
+    <StoredCheckbox key="keyboard-focus-order" label="Keyboard-Focus-Reihenfolge gleicht der des visuellen Layouts" />
     <StoredCheckbox key="no-invisible-focusable" label="Es gibt keine unsichtbaren fokusierbaren Elemente" />
     <StoredCheckbox key="buttons-space-enter" label="Buttons können über Leertaste und Eingabetaste betätigt werden" />
     <StoredCheckbox key="menus-overlays-esc" label="Menüs und Overlays können via ESC-Taste wieder geschlossen werden" />

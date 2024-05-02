28f4ac9c6cbacafa6f57582b766644b25505e936
Kai Uecker <kai.uecker@neofonie.de>
2024-05-02 14:52:27 +0200

    a11y-inspector-kai: initial commit

---
+++ src/lib/components/a11y-inspector.svelte
@@ -0,0 +1,88 @@
+<section>
+    <div class="viewport">
+        <input class="address" type="text" bind:value={src}>
+        <iframe bind:this={elmIframe} src="" title="a11y-inspector"></iframe>
+    </div>
+</section>
+
+<script>
+    import { watchedA11yContent } from '$lib/helpers/a11y-manager';
+    import { onMount } from 'svelte';
+
+    export let
+        src = 'https://google.com/';
+    
+    const
+        proxyUrl = 'http://localhost:8080/';
+
+    let
+        elmIframe,
+        inputDelay;
+
+    $:if (src) {
+        if (inputDelay) {
+            clearTimeout(inputDelay);
+        }
+        inputDelay = setTimeout(() => loadPagePerProxy(), 250);
+    }
+
+    function loadPagePerProxy() {
+        if (src && elmIframe) {
+            fetch(proxyUrl + src)
+                .then(response => response.text())
+                .then(rawHTML => {
+                    elmIframe.src = `data:text/html;charset=utf-8,${escape(rawHTML)}`;
+
+                    if (DOMParser) {
+                        const
+                            parser = new DOMParser(),
+                            doc = parser.parseFromString(rawHTML, "text/html");
+
+                        watchedA11yContent(doc);
+                    }
+                });
+        }
+    }
+
+    onMount(() => {
+        loadPagePerProxy();
+    });
+</script>
+
+<style>
+    section {
+        ---border-radius: 5px;
+        width: 100%;
+        height: 100%;
+        box-sizing: border-box;
+        padding: 1em;
+        position: relative;
+    }
+    div.viewport {
+        width: 100%;
+        height: 100%;
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        gap: .5em;
+
+        &>h4 {
+            text-align: center;
+        }
+    }
+    input.address {
+        border: none;
+        padding: 1em .5em;
+        font-size: 1.25em;
+        border-radius: var(---border-radius);
+        background-color: hsl(0deg 0% 0% / .05);
+    }
+    iframe {
+        width: 100%;
+        height: 100%;
+        border: 1px dashed hsl(0deg 0% 0% / .1);
+        border-radius: var(---border-radius);
+        box-shadow: inset 0 0 10px hsl(0deg 0% 0% / .2);
+
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,53 +1,164 @@
-<h1>Test Accessibility „A11y“ (11 stehen für 11 Buchstaben ergo eine abkürzung)</h1>
+<SplitPane
+    class="splitpane"
+    initialPosition="50%"
+>
+    <div slot="first">
+        <h1>Test Accessibility „A11y“ (11 stehen für 11 Buchstaben ergo eine abkürzung)</h1>
 
-<p>Diese Checkliste wird von unseren Entwicklern genutzt um eine gewisse Barriere-Freiheit für unsere Endkunden zu gewährleisten.
-Jede Komponente/Seite sollte gegen diese Checkliste geprüft werden.</p>
+        <p>Diese Checkliste wird von unseren Entwicklern genutzt um eine gewisse Barriere-Freiheit für unsere Endkunden zu gewährleisten.
+        Jede Komponente/Seite sollte gegen diese Checkliste geprüft werden.</p>
+        
+        <h2>Allgemein</h2>
+        
+        <label><input type="checkbox">Alle automatischen Accessibility-Tests laufen durch</label> <br />
+        <label><input type="checkbox">Axe Chrome extension</label> <br />
+        <label><input type="checkbox">Wave Chrome Erweiterung</label> <br />
+        <label><input type="checkbox">Lighthouse (Accessibility)</label> <br />
+        
+        <h2>Markup</h2>
+        
+        <label><input type="checkbox">`&lt;html />` hat korrektes lang-attribut</label> <br />
+        <label><input type="checkbox">Es gibt bei tabindex nur 0 oder -1</label> <br />
+        <label><input type="checkbox">`&lt;a />-Tag` wird für alle Links verwendet -> Seitenwechseln</label> <br />
+        <label><input type="checkbox">`&lt;button />-Tag` wird für alle Buttons/Klick/Actionflächen genutzt (kein div mit onclick etc.)</label> <br />
+        
+        <h2>Content</h2>
+        
+        <label><input type="checkbox">`&lt;button />`, `&lt;a />` und `&lt;label />` sind eindeutig und selbsterklärend</label> <br />
+        <label><input type="checkbox">Es gibt nur eine `&lt;h1 />` pro Seite</label> <br />
+        
+        <h2>Bilder</h2>
+        
+        <label><input type="checkbox">Alle `&lt;img />-Elemente` haben ein Alt-Attribut</label> <br />
+        <label><input type="checkbox">rein dekorative SVGs haben `aria-hidden="true"`</label> <br />
+        <label><input type="checkbox">`&lt;img />-Elemente` mit source SVG haben das role Attribut `role="img"`</label> <br />
+        
+        <h2>Formulare</h2>
+        
+        <label><input type="checkbox">Alle Inputs sind mit entsprechenden Label verbunden</label> <br />
+        <label><input type="checkbox">Form Input unterstützen grundsätzlich autocomplete</label> <br />
+        <label><input type="checkbox">Input Errors werden direkt mit den entsprechenden Feldern assoziiert</label> <br />
+        
+        <h2>Mobile</h2>
+        
+        <label><input type="checkbox">Die Seite kann nicht horizontal gescrollt werden</label> <br />
+        <label><input type="checkbox">Viewport Zoom ist deaktiviert</label> <br />
+        <label><input type="checkbox">Alle Klickflächen mind. `44x44px`, außer bei Inline-Elementen wie Links</label> <br />
+        
+        <h2>Tastatur</h2>
+        
+        <label><input type="checkbox">Tastatursteuerung muss komplett möglich sein</label> <br />
+        <label><input type="checkbox">Alle interaktiven Elemente müssen einen sichtbaren focustyle haben</label> <br />
+        <label><input type="checkbox">Keyboard-Focus Reihenfolge gleicht der des visuellen Layouts</label> <br />
+        <label><input type="checkbox">Es gibt keine unsichtbaren fokusierbaren Elemente</label> <br />
+        <label><input type="checkbox">Buttons können über Leertaste und Eingabetaste betätigt werden</label> <br />
+        <label><input type="checkbox">Menüs und Overlays können via ESC-Taste wieder geschlossen werden</label> <br />
+        
+        
+        <p><strong>Stand 02.2024</strong></p>
 
-<h2>Allgemein</h2>
+        <hr />
 
-<label><input type="checkbox">Alle automatischen Accessibility-Tests laufen durch</label> <br />
-<label><input type="checkbox">Axe Chrome extension</label> <br />
-<label><input type="checkbox">Wave Chrome Erweiterung</label> <br />
-<label><input type="checkbox">Lighthouse (Accessibility)</label> <br />
+        {#each Object.keys(ruleset.groups) as group }
+            <section class="a11y-group">
+                <h2>{group}</h2>
 
-<h2>Markup</h2>
+                {#each Object.keys(ruleset.groups[group]) as rule}
+                    <div class="a11y-rule">
+                        <label><input type="checkbox" use:linkA11yRule id={rule}>{ruleset.groups[group][rule].description}</label> <br />
+                    </div>
+                {/each}
+            </section>
+        {/each}
+        <div>Stand {ruleset?.lastChange || '???'}</div>
+    </div>
+    <div slot="second">
+        <A11yInspector></A11yInspector>
+    </div>
+</SplitPane>
 
-<label><input type="checkbox">`&lt;html />` hat korrektes lang-attribut</label> <br />
-<label><input type="checkbox">Es gibt bei tabindex nur 0 oder -1</label> <br />
-<label><input type="checkbox">`&lt;a />-Tag` wird für alle Links verwendet -> Seitenwechseln</label> <br />
-<label><input type="checkbox">`&lt;button />-Tag` wird für alle Buttons/Klick/Actionflächen genutzt (kein div mit onclick etc.)</label> <br />
+<script>
+    import SplitPane from '$lib/components/split-pane.svelte';
+    import A11yInspector from '$lib/components/a11y-inspector.svelte';
+    import { linkA11yRule, ruleset } from '$lib/helpers/a11y-manager.js';
+</script>
 
-<h2>Content</h2>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap');
 
-<label><input type="checkbox">`&lt;button />`, `&lt;a />` und `&lt;label />` sind eindeutig und selbsterklärend</label> <br />
-<label><input type="checkbox">Es gibt nur eine `&lt;h1 />` pro Seite</label> <br />
+    :global(:root) {
+        ---ally-rule-lineheight: 32px;
+    }
+    :global(body) {
+        margin: 0;
+        width: 100vw;
+        height: 100vh;
+        font-family: 'Ubuntu';
 
-<h2>Bilder</h2>
+        & *:has(>.--a11yRuleLinked)::after {
+            content: ' ';
+            margin: 0 .5em;
+            position: absolute;
+            top: 0;
+            right: 0;
+            width: var(---ally-rule-lineheight);
+            height: var(---ally-rule-lineheight);
+            display: inline-flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 5px;
+            background-color: hsl(0deg 0% 0% / .1);
+        }
+        & *:has(>.--a11yRuleLinked.--a11yFailed)::after {
+            content: '👎';
+            background-color: hsl(0deg 100% 50%);
+        }
+        & *:has(>.--a11yRuleLinked.--a11ySuccess)::after {
+            content: '👍';
+            background-color: hsl(120deg 100% 50%);
+        }
+    }
 
-<label><input type="checkbox">Alle `&lt;img />-Elemente` haben ein Alt-Attribut</label> <br />
-<label><input type="checkbox">rein dekorative SVGs haben `aria-hidden="true"`</label> <br />
-<label><input type="checkbox">`&lt;img />-Elemente` mit source SVG haben das role Attribut `role="img"`</label> <br />
+    /**
+     * UI
+     */
+    .splitpane {
+        width: 100%;
+        height: 100%;
+    }
+    div[slot=first],
+    div[slot=second] {
+        width: 100%;
+        height: 100%;
+    }
+    div[slot=first] {
+        padding: 1em;
+        box-sizing: border-box;
+        overflow-y: auto;
+    }
+    div[slot=second] {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 
-<h2>Formulare</h2>
+    /**
+     * Page content
+     */
+    .a11y-group {
+        box-sizing: border-box;
+        background-color: hsl(0deg 0% 0% / .05);
+        padding: .5em 1em;
+        border-radius: 5px;
+        margin-bottom: 1em;
+    }
+    .a11y-rule {
+        position: relative;
 
-<label><input type="checkbox">Alle Inputs sind mit entsprechenden Label verbunden</label> <br />
-<label><input type="checkbox">Form Input unterstützen grundsätzlich autocomplete</label> <br />
-<label><input type="checkbox">Input Errors werden direkt mit den entsprechenden Feldern assoziiert</label> <br />
-
-<h2>Mobile</h2>
-
-<label><input type="checkbox">Die Seite kann nicht horizontal gescrollt werden</label> <br />
-<label><input type="checkbox">Viewport Zoom ist deaktiviert</label> <br />
-<label><input type="checkbox">Alle Klickflächen mind. `44x44px`, außer bei Inline-Elementen wie Links</label> <br />
-
-<h2>Tastatur</h2>
-
-<label><input type="checkbox">Tastatursteuerung muss komplett möglich sein</label> <br />
-<label><input type="checkbox">Alle interaktiven Elemente müssen einen sichtbaren focustyle haben</label> <br />
-<label><input type="checkbox">Keyboard-Focus Reihenfolge gleicht der des visuellen Layouts</label> <br />
-<label><input type="checkbox">Es gibt keine unsichtbaren fokusierbaren Elemente</label> <br />
-<label><input type="checkbox">Buttons können über Leertaste und Eingabetaste betätigt werden</label> <br />
-<label><input type="checkbox">Menüs und Overlays können via ESC-Taste wieder geschlossen werden</label> <br />
-
-
-<p><strong>Stand 02.2024</strong></p>
+        & > label {
+            height: var(---ally-rule-lineheight);
+            display: flex;
+            align-items: center;
+        }
+    }
+</style>

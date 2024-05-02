7d5ab88747ff67c353ae6855e3d4f32e68d9797c
Vincent G <>
2024-05-02 11:17:37 +0200

    added event dispatcher / change handler

--- src/lib/checkbox.svelte
+++ src/lib/checkbox.svelte
@@ -1,22 +1,22 @@
+<!-- checkbox.svelte -->
 <script lang="ts">
-  import Storer from "$lib/storer.svelte";
-
-  // create unique uuid
-  export let key : string = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-
-  // define props
   export let label: string = "";
   export let checked: boolean = false;
-  export let handleChange: () => void = () => {};
 
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
   
+  function handleChange() {
+    checked = !checked;
+    console.log("checked", checked);
+    // notify parent component
+    dispatch("change", { checked });
+  }
+
 </script>
 
-<Storer bind:key={key}>
-  <input type="checkbox" bind:checked on:click={handleChange} />
-  {#if label}
-    <label for="checkbox">{label}</label>
-  {/if}
+<input type="checkbox" bind:checked id="checkbox" on:click={handleChange} />
+{#if label}
   <label for="checkbox">{label}</label>
-  <slot></slot>
-</Storer>
+{/if}

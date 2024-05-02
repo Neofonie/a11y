b9ae45b7be2ed6bb7b00022eb45368148e962cc4
Stefan Wolff <stefan.wolff@neofonie.de>
2024-05-02 13:25:04 +0200

    added tailwind and container div

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,58 +5,73 @@
 
 <Github/>
 
-<h1>Test Accessibility „A11y“ (11 stehen für 11 Buchstaben ergo eine abkürzung)</h1>
+<div class="container mx-auto px-4 py-4">
 
-<p>Diese Checkliste wird von unseren Entwicklern genutzt um eine gewisse Barriere-Freiheit für unsere Endkunden zu gewährleisten.
-Jede Komponente/Seite sollte gegen diese Checkliste geprüft werden.</p>
+    <h1 class="text-3xl font-bold">Test Accessibility „A11y“ (11 stehen für 11 Buchstaben ergo eine abkürzung)</h1>
 
-<h2>Allgemein</h2>
-<Checkbox test="123">
-    Alle automatischen Accessibility-Tests laufen durch
-    <div slot="named">huhu</div>
-</Checkbox> <br />
-<label><input type="checkbox">Axe Chrome extension</label> <br />
-<label><input type="checkbox">Wave Chrome Erweiterung</label> <br />
-<label><input type="checkbox">Lighthouse (Accessibility)</label> <br />
+    <p>Diese Checkliste wird von unseren Entwicklern genutzt um eine gewisse Barriere-Freiheit für unsere Endkunden zu
+        gewährleisten.
+        Jede Komponente/Seite sollte gegen diese Checkliste geprüft werden.</p>
 
-<h2>Markup</h2>
+    <h2>Allgemein</h2>
+    <Checkbox test="123">
+        Alle automatischen Accessibility-Tests laufen durch
+        <div slot="named">huhu</div>
+    </Checkbox> <br />
+    <label><input type="checkbox">Axe Chrome extension</label> <br />
+    <label><input type="checkbox">Wave Chrome Erweiterung</label> <br />
+    <label><input type="checkbox">Lighthouse (Accessibility)</label> <br />
 
-<label><input type="checkbox">`&lt;html />` hat korrektes lang-attribut</label> <br />
-<label><input type="checkbox">Es gibt bei tabindex nur 0 oder -1</label> <br />
-<label><input type="checkbox">`&lt;a />-Tag` wird für alle Links verwendet -> Seitenwechseln</label> <br />
-<label><input type="checkbox">`&lt;button />-Tag` wird für alle Buttons/Klick/Actionflächen genutzt (kein div mit onclick etc.)</label> <br />
+    <h2>Markup</h2>
 
-<h2>Content</h2>
+    <label><input type="checkbox">`&lt;html />` hat korrektes lang-attribut</label> <br />
+    <label><input type="checkbox">Es gibt bei tabindex nur 0 oder -1</label> <br />
+    <label><input type="checkbox">`&lt;a />-Tag` wird für alle Links verwendet -> Seitenwechseln</label> <br />
+    <label><input type="checkbox">`&lt;button />-Tag` wird für alle Buttons/Klick/Actionflächen genutzt (kein div mit
+        onclick etc.)</label> <br />
 
-<label><input type="checkbox">`&lt;button />`, `&lt;a />` und `&lt;label />` sind eindeutig und selbsterklärend</label> <br />
-<label><input type="checkbox">Es gibt nur eine `&lt;h1 />` pro Seite</label> <br />
+    <h2>Content</h2>
 
-<h2>Bilder</h2>
+    <label><input type="checkbox">`&lt;button />`, `&lt;a />` und `&lt;label />` sind eindeutig und
+        selbsterklärend</label> <br />
+    <label><input type="checkbox">Es gibt nur eine `&lt;h1 />` pro Seite</label> <br />
 
-<label><input type="checkbox">Alle `&lt;img />-Elemente` haben ein Alt-Attribut</label> <br />
-<label><input type="checkbox">rein dekorative SVGs haben `aria-hidden="true"`</label> <br />
-<label><input type="checkbox">`&lt;img />-Elemente` mit source SVG haben das role Attribut `role="img"`</label> <br />
+    <h2>Bilder</h2>
 
-<h2>Formulare</h2>
+    <label><input type="checkbox">Alle `&lt;img />-Elemente` haben ein Alt-Attribut</label> <br />
+    <label><input type="checkbox">rein dekorative SVGs haben `aria-hidden="true"`</label> <br />
+    <label><input type="checkbox">`&lt;img />-Elemente` mit source SVG haben das role Attribut `role="img"`</label>
+    <br />
 
-<label><input type="checkbox">Alle Inputs sind mit entsprechenden Label verbunden</label> <br />
-<label><input type="checkbox">Form Input unterstützen grundsätzlich autocomplete</label> <br />
-<label><input type="checkbox">Input Errors werden direkt mit den entsprechenden Feldern assoziiert</label> <br />
+    <h2>Formulare</h2>
 
-<h2>Mobile</h2>
+    <label><input type="checkbox">Alle Inputs sind mit entsprechenden Label verbunden</label> <br />
+    <label><input type="checkbox">Form Input unterstützen grundsätzlich autocomplete</label> <br />
+    <label><input type="checkbox">Input Errors werden direkt mit den entsprechenden Feldern assoziiert</label> <br />
 
-<label><input type="checkbox">Die Seite kann nicht horizontal gescrollt werden</label> <br />
-<label><input type="checkbox">Viewport Zoom ist deaktiviert</label> <br />
-<label><input type="checkbox">Alle Klickflächen mind. `44x44px`, außer bei Inline-Elementen wie Links</label> <br />
+    <h2>Mobile</h2>
 
-<h2>Tastatur</h2>
+    <label><input type="checkbox">Die Seite kann nicht horizontal gescrollt werden</label> <br />
+    <label><input type="checkbox">Viewport Zoom ist deaktiviert</label> <br />
+    <label><input type="checkbox">Alle Klickflächen mind. `44x44px`, außer bei Inline-Elementen wie Links</label> <br />
 
-<label><input type="checkbox">Tastatursteuerung muss komplett möglich sein</label> <br />
-<label><input type="checkbox">Alle interaktiven Elemente müssen einen sichtbaren focustyle haben</label> <br />
-<label><input type="checkbox">Keyboard-Focus Reihenfolge gleicht der des visuellen Layouts</label> <br />
-<label><input type="checkbox">Es gibt keine unsichtbaren fokusierbaren Elemente</label> <br />
-<label><input type="checkbox">Buttons können über Leertaste und Eingabetaste betätigt werden</label> <br />
-<label><input type="checkbox">Menüs und Overlays können via ESC-Taste wieder geschlossen werden</label> <br />
+    <h2>Tastatur</h2>
+
+    <label><input type="checkbox">Tastatursteuerung muss komplett möglich sein</label> <br />
+    <label><input type="checkbox">Alle interaktiven Elemente müssen einen sichtbaren focustyle haben</label> <br />
+    <label><input type="checkbox">Keyboard-Focus Reihenfolge gleicht der des visuellen Layouts</label> <br />
+    <label><input type="checkbox">Es gibt keine unsichtbaren fokusierbaren Elemente</label> <br />
+    <label><input type="checkbox">Buttons können über Leertaste und Eingabetaste betätigt werden</label> <br />
+    <label><input type="checkbox">Menüs und Overlays können via ESC-Taste wieder geschlossen werden</label> <br />
 
 
-<p><strong>Stand 02.2024</strong></p>
+    <p><strong>Stand 02.2024</strong></p>
+</div>
+
+
+
+
+
+<style lang="postcss">
+    :global(html) {}
+</style>

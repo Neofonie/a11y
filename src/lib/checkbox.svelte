22943af63e6a8b2031cba0c531341130271ae0d6
Vincent G <>
2024-05-02 11:26:02 +0200

    Update checkbox component to use value instead of checked

--- src/lib/checkbox.svelte
+++ src/lib/checkbox.svelte
@@ -1,22 +1,23 @@
 <!-- checkbox.svelte -->
 <script lang="ts">
   export let label: string = "";
-  export let checked: boolean = false;
+  export let value: boolean = false;
+  
 
   import { createEventDispatcher } from "svelte";
 
   const dispatch = createEventDispatcher();
   
   function handleChange() {
-    checked = !checked;
-    console.log("checked", checked);
+    value = !value;
+    console.log("checked", value);
     // notify parent component
-    dispatch("change", { checked });
+    dispatch("change", { checked: value });
   }
 
 </script>
 
-<input type="checkbox" bind:checked id="checkbox" on:click={handleChange} />
+<input type="checkbox" bind:checked={value} id="checkbox" on:click={handleChange} />
 {#if label}
   <label for="checkbox">{label}</label>
 {:else}

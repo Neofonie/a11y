6378232b2db4dd7b8cfb10fe1e405b209b232b30
vincentkg <vincent.gross@neofonie.de>
2024-05-17 18:35:26 +0200

    disabled daisy base styles, used formatter

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,16 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
+  plugins: [require("daisyui")],
   //prefix: "tw-",
   daisyui: {
     themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: 'dark', // name of one of the included themes for dark mode
-    base: true, // applies background color and foreground color for root element by default
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: false, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
-    prefix: 'd-', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    prefix: "d-", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
     logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ':root' // The element that receives theme color CSS variables
-  }
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
 };

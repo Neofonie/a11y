0c95decf4afaaede298154a577a0010f7161ee65
Willy Woitas <dutscher_sbf@hotmail.com>
2024-06-24 23:30:02 +0200

    Add aria patterns

--- src/lib/content/links.svelte
+++ src/lib/content/links.svelte
@@ -8,6 +8,7 @@ import Headline from "$lib/components/headline.svelte";
 <Link href="https://www.w3schools.com/accessibility/index.php">W3School Accessibility Tutorial</Link><br/>
 <Link href="https://developer.mozilla.org/en-US/docs/Learn/Accessibility">MDN Dev Docs: Accessibility</Link><br/>
 <Link href="https://www.w3.org/TR/WCAG20/">Web Content Accessibility Guidelines (WCAG)</Link><br/>
+<Link href="https://www.w3.org/WAI/ARIA/apg/patterns/">ARIA Components Patterns</Link><br/>
 <Link href="https://www.barrierefreiheit-dienstekonsolidierung.bund.de/Webs/PB/DE/barrierefreie_it/digitale-barrierefreiheit/digitale-barrierefreiheit-node.html">
     Digitale Barrierefreiheit (Bundesministerium des Innern und für Heimat)
 </Link><br/>
